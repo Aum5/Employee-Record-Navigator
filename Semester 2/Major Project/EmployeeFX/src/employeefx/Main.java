ac215586ec4148beb7411183018e1f5d5c935936
Aum <thakkaau@sheridancollege.ca>
2023-03-30 16:51:36 -0400

    design sample

--- Semester 2/Major Project/EmployeeFX/src/employeefx/Main.java
+++ Semester 2/Major Project/EmployeeFX/src/employeefx/Main.java
@@ -3,11 +3,18 @@ package employeefx;
 import content.Employee;
 import java.util.LinkedList;
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -41,22 +48,86 @@ public class Main extends Application{
         stage.show();
     }
    
-    private GridPane employeePane() {
-        GridPane pane = new GridPane();
-        pane.add(btnFirst, 2, 0);
-       pane.add(lblID, 0, 1);
-        pane.add(txtID, 1, 1);
-        pane.add(lblName, 0, 2);
-        pane.add(txtName, 1, 2);
-        pane.add(lblcity, 0, 3);
-        pane.add(txtcity, 1, 3);
-        pane.add(lblpos, 0, 4);
-        pane.add(txtpos, 1, 4);
-        pane.add(btnAdd, 1, 9);
-        return pane;
-    }
-    
-    
+   private GridPane employeePane() {
+    GridPane grid = new GridPane();
+    grid.setHgap(10);
+    grid.setVgap(10);
+    grid.setPadding(new Insets(10));
+
+    // Add navigation buttons to top center
+    HBox navButtons = new HBox(50);
+    navButtons.setAlignment(Pos.CENTER);
+    navButtons.getChildren().addAll(btnFirst, btnNext, btnpre, btnLast);
+    grid.add(navButtons, 0, 0, 4, 1);
+
+    // Add labels and text fields
+    grid.add(lblID, 0, 1);
+    grid.add(txtID, 1, 1);
+    grid.add(lblName, 2, 1);
+    grid.add(txtName, 3, 1);
+    grid.add(lblcity, 0, 2);
+    grid.add(txtcity, 1, 2);
+    grid.add(lblpos, 2, 2);
+    grid.add(txtpos, 3, 2);
+
+    // Add search button below navigation buttons
+    HBox searchBox = new HBox(10);
+    searchBox.setAlignment(Pos.CENTER);
+    searchBox.getChildren().addAll( btnsearch);
+    grid.add(searchBox, 0, 3, 4, 1);
+
+    // Add add/delete/update buttons to bottom center
+    HBox editButtons = new HBox(50);
+    editButtons.setAlignment(Pos.CENTER);
+    editButtons.getChildren().addAll(btnAdd, btnDelete, btnUp);
+    GridPane.setHalignment(editButtons, HPos.CENTER);
+    grid.add(editButtons, 0, 4, 4, 1);
+
+    // Set column constraints
+    ColumnConstraints col1 = new ColumnConstraints();
+    col1.setHalignment(HPos.RIGHT);
+    col1.setPrefWidth(120);
+    grid.getColumnConstraints().add(col1);
+
+    ColumnConstraints col2 = new ColumnConstraints();
+    col2.setHalignment(HPos.LEFT);
+    col2.setPrefWidth(220);
+    grid.getColumnConstraints().add(col2);
+
+    ColumnConstraints col3 = new ColumnConstraints();
+    col3.setHalignment(HPos.RIGHT);
+    col3.setPrefWidth(120);
+    grid.getColumnConstraints().add(col3);
+
+    ColumnConstraints col4 = new ColumnConstraints();
+    col4.setHalignment(HPos.LEFT);
+    col4.setPrefWidth(220);
+    grid.getColumnConstraints().add(col4);
+
+    // Set row constraints
+    RowConstraints row0 = new RowConstraints();
+    row0.setPrefHeight(50);
+    grid.getRowConstraints().add(row0);
+
+    RowConstraints row1 = new RowConstraints();
+    row1.setPrefHeight(60);
+    grid.getRowConstraints().add(row1);
+
+    RowConstraints row2 = new RowConstraints();
+    row2.setPrefHeight(60);
+    grid.getRowConstraints().add(row2);
+
+    RowConstraints row3 = new RowConstraints();
+    row3.setPrefHeight(50);
+    grid.getRowConstraints().add(row3);
+
+    RowConstraints row4 = new RowConstraints();
+    row4.setPrefHeight(50);
+    grid.getRowConstraints().add(row4);
+
+    return grid;
+}
+ 
     public static void main(String[] args) {
      Application.launch(args);    
     }    
